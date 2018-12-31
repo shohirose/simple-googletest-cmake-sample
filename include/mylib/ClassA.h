818acd3ba48786c04f63654227d5c400b3bcf04e
Sho Hirose <sho.hirose@utexas.edu>
2018-12-31 00:17:40 -0600

    fix: add include guards

--- include/mylib/ClassA.h
+++ include/mylib/ClassA.h
@@ -1,8 +1,12 @@
-class ClassA
-{
-public:
-	ClassA() = default;
-	~ClassA() = default;
-	int sumA1(int x, int y);
-	int sumA2(int x, int y);
+#ifndef SIMPLE_GOOGLETEST_CMAKE_SAMPLE_INCLUDE_MYLIB_CLASS_A_H
+#define SIMPLE_GOOGLETEST_CMAKE_SAMPLE_INCLUDE_MYLIB_CLASS_A_H
+
+class ClassA {
+ public:
+  ClassA()  = default;
+  ~ClassA() = default;
+  int sumA1(int x, int y);
+  int sumA2(int x, int y);
 };
+
+#endif
--- include/mylib/ClassB.h
+++ include/mylib/ClassB.h
@@ -1,8 +1,12 @@
-class ClassB
-{
-public:
-	ClassB() = default;
-	~ClassB() = default;
-	int sumB1(int x, int y);
-	int sumB2(int x, int y);
+#ifndef SIMPLE_GOOGLETEST_CMAKE_SAMPLE_INCLUDE_MYLIB_CLASS_B_H
+#define SIMPLE_GOOGLETEST_CMAKE_SAMPLE_INCLUDE_MYLIB_CLASS_B_H
+
+class ClassB {
+ public:
+  ClassB()  = default;
+  ~ClassB() = default;
+  int sumB1(int x, int y);
+  int sumB2(int x, int y);
 };
+
+#endif
